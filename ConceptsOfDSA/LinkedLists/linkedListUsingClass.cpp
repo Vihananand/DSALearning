c24de1ca1ac459aab96a5a87d4c05319a1f09961
Vihananand <vihananand2018@gmail.com>
2024-09-20 01:04:38 +0530

    updated linked list

--- ConceptsOfDSA/LinkedLists/linkedListUsingClass.cpp
+++ ConceptsOfDSA/LinkedLists/linkedListUsingClass.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class node{
 public:
     int data;
-    node* next;
+    node *next;
 
     node(int val){
         data = val;
@@ -12,15 +12,15 @@ public:
     }
 }; 
 
-void insertAtTail(node* &head, int val){
-    node* n = new node(val);
+void insertAtTail(node *&head, int val){
+    node *n = new node(val);
 
     if(head == NULL){
         head = n;
         return;
     }
 
-    node* temp = head;
+    node *temp = head;
     while (temp->next != NULL)
     {
         temp = temp->next;
@@ -28,14 +28,43 @@ void insertAtTail(node* &head, int val){
     temp->next = n;
 }
 
-void insertAtHead(node* &head, int val){
-    node* n = new node(val);
+void insertAtHead(node *&head, int val){
+    node *n = new node(val);
     n->next = head;
     head = n;
 }
 
-void dispaly(node* head){
-    node* temp = head;
+void deleteHead(node *&head){
+    node *toDelete = head;
+    head = head->next;
+
+    delete toDelete;
+}
+
+void deletion(node *&head, int val){
+
+    if(head == NULL){
+        return;
+    }
+
+    if(head->next == NULL){
+        deleteHead(head);
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *toDelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete toDelete;
+}
+
+void dispaly(node *head){
+    node *temp = head;
     while (temp != NULL)
     {
         cout << temp->data << "->";
@@ -46,7 +75,7 @@ void dispaly(node* head){
 
 int main(){
 
-    node* head = NULL;
+    node *head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);
