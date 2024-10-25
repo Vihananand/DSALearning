bc89beba5fb1fe6deee75cc89aa208a4dbe25c0b
Vihananand <vihananand2018@gmail.com>
2024-10-25 19:34:21 +0530

    stack using LL added

--- ConceptsOfDSA/Stack/StackNotes.md
+++ ConceptsOfDSA/Stack/StackNotes.md
@@ -57,3 +57,93 @@ bool empty() {
     }
 ~~~
 ---
+### Stack Implementation using Linked List
+
+### 1. The head of the linked list will point to the head of stack.
+
+```c++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+class Stack{
+    int stacksize;
+    Node* head;
+
+public:
+    Stack(){
+        head = NULL;
+        stacksize = 0;
+    }
+
+    void push(int val){
+
+        Node* n = new Node();
+        n->data = val;
+        n->next = head;
+        head = n;
+        stacksize++;
+
+        cout << "Element " << val << " pushed to stack." << endl;
+    }
+
+    void pop(){
+        if(head == NULL){
+            cout << "Stack Empty!" << endl;
+            return;
+        }
+
+        Node* n = head;
+
+        head = n->next;
+        n->next = NULL;
+        delete n;
+        stacksize--;
+
+        cout << "Element " << n->data << " Popped!" << endl;
+    }
+
+    int top(){
+        if(head == NULL){
+            cout << "No top Element! Stack is Empty!";
+            return -1;
+        }
+        return head->data;
+    }
+    
+    void size(){
+        cout << "Size of the stack is " << stacksize << endl;
+    }
+
+    int empty(){
+        if(head == NULL){
+            cout << "Stack Empty!" << endl;
+            return 1;
+        }
+        cout << "Stack is not empty!" << endl;
+        return 0;  
+    }
+
+};
+
+int main(){
+    Stack s;
+
+    s.push(1);
+    s.push(4);
+    s.push(6);
+
+    s.size();
+
+    s.empty();
+
+    s.pop();
+    s.pop();
+
+    return 0;
+}
+```
--- ConceptsOfDSA/Stack/stackUsingLL.cpp
+++ ConceptsOfDSA/Stack/stackUsingLL.cpp
@@ -4,33 +4,87 @@ using namespace std;
 struct Node{
     int data;
     struct Node* next;
-
-    Node(int data){
-        this->data = data;
-        next = NULL;
-    }
 };
 
-struct Node* push(struct Node* top, int val){
-    if(top == NULL){
-        cout << "Stack Empty!" << endl;
-    }
-    struct Node* n = new Node(val);
-    n->data = val;
-    n->next = top;
-    top = n;
-    return top;
-}
+class Stack{
+    int stacksize;
+    Node* head;
 
-int isEmpty(struct Node* top){
-    if(top == NULL){
-        return 1;
+public:
+    Stack(){
+        head = NULL;
+        stacksize = 0;
     }
-    return 0;
-}
+
+    void push(int val){
+
+        Node* n = new Node();
+        n->data = val;
+        n->next = head;
+        head = n;
+        stacksize++;
+
+        cout << "Element " << val << " pushed to stack." << endl;
+    }
+
+    void pop(){
+        if(head == NULL){
+            cout << "Stack Empty!" << endl;
+            return;
+        }
+
+        Node* n = head;
+
+        head = n->next;
+        n->next = NULL;
+        delete n;
+        stacksize--;
+
+        cout << "Element " << n->data << " Popped!" << endl;
+    }
+
+    int top(){
+        if(head == NULL){
+            cout << "No top Element! Stack is Empty!";
+            return -1;
+        }
+        return head->data;
+    }
+    
+    void size(){
+        cout << "Size of the stack is " << stacksize << endl;
+    }
+
+    int empty(){
+        if(head == NULL){
+            cout << "Stack Empty!" << endl;
+            return 1;
+        }
+        cout << "Stack is not empty!" << endl;
+        return 0;  
+    }
+
+};
 
 int main(){
-    struct Node* top = NULL;
-    push(top, 78);
+    Stack s;
+
+    s.push(1);
+    s.push(4);
+    s.push(6);
+    s.push(9);
+    s.push(11);
+
+    s.size();
+
+    s.empty();
+
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+
     return 0;
 }
