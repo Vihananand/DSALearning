053251206267d9994d066eeeb8e5f82c44bd7224
Vihananand <vihananand2018@gmail.com>
2024-10-25 22:27:36 +0530

    updated BST

--- ConceptsOfDSA/Trees/binarySearchTreee.cpp
+++ ConceptsOfDSA/Trees/binarySearchTreee.cpp
@@ -35,6 +35,40 @@ void inOrder(Node* root){
     inOrder(root->right);
 }
 
+Node* findMin(Node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int key) {
+    if (root == NULL) return root;
+
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->data) {
+        root->right = deleteNode(root->right, key);
+    }
+    else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
 int main(){
     Node* root = NULL;
     root = insertBST(root, 5);
