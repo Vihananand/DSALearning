102116d563fc30aca380504f70bc359399b65c79
“vihananand2018@gmail.com” <vihananand2018@gmail.com>
2024-11-13 20:21:49 +0530

    updated heap programs

--- ConceptsOfDSA/Heap/creatingMinHeap.cpp
+++ ConceptsOfDSA/Heap/creatingMinHeap.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Insert(int A[], int n) {
-    int temp = A[n];
-    int i = n;
+void Insert(int A[], int i) {
+    int temp = A[i];
 
-    while (i > 1 && temp < A[i / 2]) {
+    while (i > 0 && temp < A[i / 2]) {
         A[i] = A[i / 2];
         i = i / 2;
     }
@@ -13,15 +12,15 @@ void Insert(int A[], int n) {
 }
 
 void createAndDisplayHeap() {
-    int A[] = {0, 3, 5, 1, 10, 2, 7};
+    int A[] = {3, 5, 1, 10, 2, 7};
     int size = 6;
 
-    for (int i = 2; i <= size; i++) {
+    for (int i = 0; i < size; i++) {
         Insert(A, i);
     }
 
     cout << "Heap is: ";
-    for (int i = 1; i <= size; i++) {
+    for (int i = 0; i < size; i++) {
         cout << A[i] << " ";
     }
 }
