38584f97d6b705d9432b2890c0e6a5cc84c78517
“vihananand2018@gmail.com” <vihananand2018@gmail.com>
2024-11-18 23:37:35 +0530

    deleting root element updated

--- ConceptsOfDSA/Heap/deletingRootElement.cpp
+++ ConceptsOfDSA/Heap/deletingRootElement.cpp
@@ -3,65 +3,66 @@ using namespace std;
 
 void Insert(int A[], int n) {
     int temp = A[n];
-    int i = n;
 
-    while (i > 1 && temp > A[i / 2]) {
-        A[i] = A[i / 2];
-        i = i / 2;
+    while (n > 0 && temp > A[(n - 1) / 2]) {
+        A[n] = A[(n - 1) / 2];
+        n = (n - 1) / 2;
     }
-    A[i] = temp;
+
+    A[n] = temp;
 }
 
 void DisplayHeap(int A[], int size) {
     cout << "Heap is: ";
-    for (int i = 1; i <= size; i++) {
+    for (int i = 0; i < size; i++) {
         cout << A[i] << " ";
     }
     cout << endl;
 }
 
-int Delete(int A[], int n) {
+int Delete(int A[], int &n) {
     int i, j, x, temp, val;
-    val = A[1];
 
-    x = A[n];
-    A[1] = A[n];
+    val = A[0];
+    x = A[--n];
+    A[0] = x;
+    
 
-    i = 1; 
-    j = 2 * i;
+    i = 0;
+    j = 2 * i + 1;
 
-    while (j < n - 1)
-    {
-        if(A[j + 1] > A[j]){
+    while (j < n) {
+        if (j + 1 < n && A[j + 1] > A[j]) {
             j = j + 1;
         }
-        if(A[i] < A[j]){
+
+        if (A[i] < A[j]) {
             temp = A[i];
             A[i] = A[j];
             A[j] = temp;
 
             i = j;
-            j = j * 2;
-        }
-        else{
+            j = 2 * i + 1;
+        } else {
             break;
         }
     }
-    return val; 
+
+    return val;
 }
 
 int main() {
-    int A[] = {0, 3, 5, 1, 10, 2, 7};
+    int A[] = {3, 5, 1, 10, 2, 7};
     int size = 6;
 
-    for (int i = 2; i <= size; i++) {
+    for (int i = 1; i < size; i++) {
         Insert(A, i);
     }
 
     cout << "Initial ";
     DisplayHeap(A, size);
 
-    int deletedValue = Delete(A, 5);
+    int deletedValue = Delete(A, size);
     cout << "Deleted value: " << deletedValue << " " << endl;
 
     cout << "After deletion ";
