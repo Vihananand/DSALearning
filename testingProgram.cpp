1cded0a7285c58e806864f8de5e1657e63b70274
Vihananand <vihananand2018@gmail.com>
2024-11-26 19:18:55 +0530

    updated testing programs

--- testingProgram.cpp
+++ testingProgram.cpp
@@ -1,46 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void buildMaxHeap(int arr[], int n) {
-    priority_queue<int> maxHeap;
 
-    for (int i = 0; i < n; i++) {
-        maxHeap.push(arr[i]);
+int main(){
+    int arr[100][100];
+    int size;
+    cin >> size;
+    for(int i = 0; i < size; i++){
+        for (int j = 0; i < size; j++){
+            cin >> arr[i][j];
+        }  
     }
 
-    while (!maxHeap.empty()) {
-        cout << maxHeap.top() << " ";
-        maxHeap.pop();
+    for(int i = 0; i < size; i++){
+        for (int j = 0; i < size; j++){
+            cout << arr[i][j] << " ";
+        } 
+        cout << endl; 
     }
-    cout << endl;
-}
-
-void buildMinHeap(int arr[], int n) {
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-
-    for (int i = 0; i < n; i++) {
-        minHeap.push(arr[i]);
-    }
-
-    while (!minHeap.empty()) {
-        cout << minHeap.top() << " ";
-        minHeap.pop();
-    }
-    cout << endl;
-}
-
-int main() {
-    int arr[] = {15, 10, 20, 5, 8};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Input Elements: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    buildMaxHeap(arr, n); 
-    buildMinHeap(arr, n);
-
-    return 0;
 }
